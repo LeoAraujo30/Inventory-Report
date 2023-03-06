374a4dd2932a99f489f406c09bf3a4adf3948d45
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2023-03-06 18:25:05 -0300

    requisitos 1 8 e 9 feitos

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,37 @@
-# from inventory_report.inventory.product import Product
+import pytest
+from inventory_report.inventory.product import Product
 
 
-def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def dict():
+    return {
+        "id": 1,
+        "nome_do_produto": "Nicotine Polacrilex",
+        "nome_da_empresa": "Target Corporation",
+        "data_de_fabricacao": "2021-02-18",
+        "data_de_validade": "2023-09-17",
+        "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+        "instrucoes_de_armazenamento": "instrucao 1"
+    }
+
+
+def test_cria_produto(dict):
+    product = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1"
+    )
+
+    assert product.id == dict["id"]
+    assert product.nome_do_produto == dict["nome_do_produto"]
+    assert product.nome_da_empresa == dict["nome_da_empresa"]
+    assert product.data_de_fabricacao == dict["data_de_fabricacao"]
+    assert product.data_de_validade == dict["data_de_validade"]
+    assert product.numero_de_serie == dict["numero_de_serie"]
+    assert product.instrucoes_de_armazenamento == dict[
+        "instrucoes_de_armazenamento"
+    ]
--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,51 @@
-# from inventory_report.reports.colored_report import ColoredReport
+import pytest
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.colored_report import ColoredReport
 
 
-def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def list():
+    return [
+        {
+            "id": "1",
+            "nome_do_produto": "Nicotine Polacrilex",
+            "nome_da_empresa": "Target Corporation",
+            "data_de_fabricacao": "2021-02-18",
+            "data_de_validade": "2023-09-17",
+            "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+            "instrucoes_de_armazenamento": "instrucao 1"
+        },
+        {
+            "id": "2",
+            "nome_do_produto": "fentanyl citrate",
+            "nome_da_empresa": "Corporation",
+            "data_de_fabricacao": "2020-12-06",
+            "data_de_validade": "2023-12-25",
+            "numero_de_serie": "FR29 5951 7573 74OY XKGX 6CSG D20",
+            "instrucoes_de_armazenamento": "instrucao 2"
+        },
+        {
+            "id": "3",
+            "nome_do_produto": "Nicotine Polacrilex",
+            "nome_da_empresa": "Corporation",
+            "data_de_fabricacao": "2021-12-06",
+            "data_de_validade": "2024-12-25",
+            "numero_de_serie": "FR29 5951 7573 74OY XKGX 6CSG D20",
+            "instrucoes_de_armazenamento": "instrucao 2"
+        },
+    ]
+
+
+def test_decorar_relatorio(list):
+    colored_report = ColoredReport(SimpleReport)
+
+    colored_string = (
+        "\033[32mData de fabricação mais antiga:\033[0m"
+        " \033[36m2020-12-06\033[0m\n"
+        "\033[32mData de validade mais próxima:\033[0m"
+        " \033[36m2023-09-17\033[0m\n"
+        "\033[32mEmpresa com mais produtos:\033[0m"
+        " \033[31mCorporation\033[0m"
+    )
+
+    assert colored_report.generate(list) == colored_string
